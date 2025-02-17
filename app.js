c98eb3c2f6a6d72662bc285a427e7aff104f0d4d
EstherElise <esther.elise.g@gmail.com>
2025-02-17 10:30:46 +0100

    Lagt til karakter, fiende og kamp

--- app.js
+++ app.js
@@ -14,35 +14,189 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 const heroNameInput = document.getElementById("character-name");
-const heroName = heroNameInput.value;
 const heroHpInput = document.getElementById("character-hp");
-const heroHp = heroHpInput.value;
 const heroAdInput = document.getElementById("attack-damage");
-const heroAd = heroAdInput.value;
+//Henter alle bildene med alternativ tekst "Bilde"
+const heroImage = document.querySelectorAll("[alt^='Bilde']");
+//Valgt bilde
+let selectedHero;
+
+//Henter knappene som legger til helt og fiende
 const btnCreateChar = document.getElementById("create-character");
+const btnGenerateEnemy = document.getElementById("generate-enemy");
+//Henter knapp som starter kampen
+const btnStartFight = document.getElementById("start-fight");
 
-//Array med profilbildene til heltene. Minus én for å fjerne det ene fiendebildet som delte classname
-let heroImageGallery = document.getElementsByClassName("profile-img");
-let remove = 1;
-let removeImage = (heroImageGallery, remove) => {
-  const copyImageGallery = [...heroImageGallery];
-  for (let i = 0; i < remove; i++) {
-    copyImageGallery.pop();
-  }
-  return copyImageGallery;
+//Henter kampområdet
+const battleArena = document.getElementById("battle-area");
+
+//Resetter kampen
+function resetFight() {
+  document.getElementById("battle-area").textContent = "";
 };
-let newImageGallery = removeImage(heroImageGallery, remove);
-
-console.log(newImageGallery);
 
 
+//Funksjon som gjør at bildet man trykker på blir valgt
+heroImage.forEach(function (heroImage) {
+  heroImage.addEventListener("click", function () {
+  selectedHero = heroImage.attributes.alt.value
+  });
+});
+
+//Henter verdiene fra inputfeltene og legger inn i localStorage
 function createChar() {
-  const character = { Navn: heroName, HP: heroHp, Attackpower: heroAd }
+  const character = { 
+    Navn: heroNameInput.value, 
+    HP: Math.floor(heroHpInput.value), 
+    Angrepsstyrke: Math.floor(heroAdInput.value), 
+    Profilbilde: selectedHero 
+  };
   localStorage.setItem("Helt", JSON.stringify(character))
-  heroName
+  displayHero();
+  // generateEnemy();
+  // resetFight();
 };
 
-btnCreateChar.addEventListener("click", createChar);
+//Profilkortet til helten som blir synlig på nettsiden
+function displayHero() {
+  //Henter helt fra localStorage
+  const getHero = localStorage.getItem("Helt");
+  const hero = JSON.parse(getHero);
 
+  // //Mapping av alt tekst til bildefil
+  // const heroImageMap = {
+  //   "Bilde 1": "/assets/death-knight.jpg",
+  //   "Bilde 2": "/assets/hunter.jpg",
+  //   "Bilde 3": "/assets/mage.jpg"
+  // };
 
+  // const getImageAltText = localStorage.getItem("Helt");
+  // const imageAltText = JSON.parse(getImageAltText);
+
+  // const chosenHeroImage = heroImageMap[imageAltText];
+
+  battleArena.innerhtml = "";
+    const divHero = document.createElement("div");
+    divHero.setAttribute("id", "character-display");
+    divHero.className = "profile-card";
+
+    const headingHero = document.createElement("h2");
+    headingHero.textContent = "Helt";
+
+    //Midlertidig bilde. Sliter med å hente fra localStorage
+    const imgHero = document.createElement("img");
+    imgHero.setAttribute("id", "char-img");
+    imgHero.alt = "Profilbilde";
+    imgHero.src = "assets/hunter.jpg";
+
+    const charName = document.createElement("p");
+    charName.setAttribute("id", "char-name");
+    charName.textContent = `Navn: ${hero.Navn}`;
+
+    const charHP = document.createElement("p");
+    charHP.setAttribute("id", "char-hp");
+    charHP.textContent = `HP: ${hero.HP}`;
+
+    const charAttack = document.createElement("p");
+    charAttack.setAttribute("id", "char-attack");
+    charAttack.textContent = `Angrepsstyrke: ${hero.Angrepsstyrke}`;
+
+    battleArena.appendChild(divHero);
+    divHero.append(headingHero, imgHero, charName, charHP, charAttack);
+  };
+
+//Knapp: Lager karakter og legger den på battlearenaen
+btnCreateChar.addEventListener("click", (e) => {
+  createChar()
+});
+
+//Genererer tilfeldig fiende og legger til i localStorage
+function generateEnemy() {
+  const enemies = [
+    { name: "Sumpmonster", image: "assets/swamp-monster.jpg" },
+    { name: "Drage", image: "assets/dragon.jpg" },
+    { name: "Orc", image: "assets/monster.jpg" }
+  ]
+
+  const enemy = enemies[Math.floor(Math.random() * enemies.length)];
+  const hpEnemy = Math.floor(Math.random() * (150 - 50 + 1)) + 50;
+  const attackEnemy = Math.floor(Math.random() * (40 - 10 + 1)) + 10;
+
+  const generatedEnemy = {
+    Navn: enemy.name,
+    HP: hpEnemy,
+    Angrepsstyrke: attackEnemy,
+    Profilbilde: enemy.image
+  };
+  localStorage.setItem("Fiende", JSON.stringify(generatedEnemy));
+  displayEnemy();
+  // displayHero();
+  // resetFight();
+};
+
+function displayEnemy() {
+  const getEnemy = localStorage.getItem("Fiende");
+  const chosenEnemy = JSON.parse(getEnemy);
+  battleArena.innerhtml = "";
+  const divEnemy = document.createElement("div");
+  divEnemy.setAttribute("id", "enemy-fight-display");
+  divEnemy.className = "profile-card";
+
+  const headingEnemy = document.createElement("h2");
+  headingEnemy.textContent = "Fiende";
+
+  const imgEnemy = document.createElement("img");
+  imgEnemy.setAttribute("id", "enemy-fight-img");
+  imgEnemy.alt = "Fiendens profilbilde";
+  imgEnemy.src = chosenEnemy.Profilbilde;
+
+  const enemyName = document.createElement("p");
+  enemyName.setAttribute("id", "enemy-fight-name");
+  enemyName.textContent = `Navn: ${chosenEnemy.Navn}`;
+
+  const enemyHP = document.createElement("p");
+  enemyHP.setAttribute("id", "enemy-fight-hp");
+  enemyHP.textContent = `HP: ${chosenEnemy.HP}`;
+
+  const enemyAttack = document.createElement("p");
+  enemyAttack.setAttribute("id", "enemy-fight-attack");
+  enemyAttack.textContent = `Angrepsstyrke: ${chosenEnemy.Angrepsstyrke}`;
+
+  battleArena.appendChild(divEnemy);
+  divEnemy.append(headingEnemy, imgEnemy, enemyName, enemyHP, enemyAttack);
+};
+
+//Knapp: Generer fiende
+btnGenerateEnemy.addEventListener("click", (e) => {
+  generateEnemy()
+});
+
+//Array med mulige resultater
+const results = [
+  "Du vant!",
+  "Du tapte",
+  "Uavgjort"
+]
+
+//Kampen
+function fight () {
+  const result = document.getElementById("battle-result");
+  const getHero = localStorage.getItem("Helt");
+  const hero = JSON.parse(getHero);
+  const getEnemy = localStorage.getItem("Fiende");
+  const chosenEnemy = JSON.parse(getEnemy);
+
+  if (hero.HP > chosenEnemy.HP) {
+    result.textContent = results[0];
+  }
+  else if (chosenEnemy.HP > hero.HP) {
+    result.textContent = results[1];
+  }
+  else if (hero.HP = chosenEnemy.HP) {
+    result.textContent = results[3];
+  }
+}
+
+//Knapp: sammenligner HP
+btnStartFight.addEventListener("click", fight)
 });
--- tests/battle.test.js
+++ tests/battle.test.js
@@ -1,1 +1,28 @@
 // Her kan du skrive testene dine
+
+
+//Opprette 3 ulike heros og enemies; en hvor hero vinner, en hvor hero taper, og en hvor det blir likt
+
+const { fight } = require("./app.js");
+  const hero = {
+    Name: "Arhi",
+    Hp: "130",
+    Attackpower: "50",
+  };
+
+  const enemy = {
+    Name: "Or",
+    Hp: "100",
+    Attackpower: "60",
+  };
+test ("Burde returnere true dersom heroHp er høyere enn enemyHp", () => {
+  expect(fight(hero.Hp > enemy.Hp)).toBe(true);  
+});
+
+test ("Burde returnere true dersom enemyHp er høyere enn heroHp", () => {
+  expect(fight(hero.Hp < enemy.Hp)).toBe(true);
+});
+
+test ("Burde returnere true dersom enemyHp er høyere enn heroHp", () => {
+  expect(fight(hero.Hp = enemy.Hp)).toBe();
+});
